658d9206057423890df6898f548c91ec26c6b44b
Christian Wesselhoeft <hi@xtian.us>
2020-09-11 15:28:58 -0600

    Introduce Opcode enum (#5)
    * Introduce Opcode enum
    * common.zig: Make Opcode non-exhaustive
    * common.zig: Add explicit values to Opcode enum

--- src/base/common.zig
+++ src/base/common.zig
@@ -10,9 +10,19 @@ pub const MessageHeader = struct {
     rsv1: bool = false,
     rsv2: bool = false,
     rsv3: bool = false,
-    opcode: u4,
+    opcode: Opcode,
     length: u64,
     mask: ?u32 = null,
+};
+
+pub const Opcode = enum(u4) {
+    Continuation = 0x00,
+    Text = 0x01,
+    Binary = 0x02,
+    Close = 0x08,
+    Ping = 0x09,
+    Pong = 0x0a,
+    _,
 };
 
 pub const ChunkEvent = struct {
