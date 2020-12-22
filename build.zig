09373be6718548164a1ab2d1cc45cc684e5e47c7
Nameless <truemedian@gmail.com>
2020-12-21 21:07:39 -0600

    support zkg

--- build.zig
+++ build.zig
@@ -2,17 +2,17 @@ const std = @import("std");
 const Builder = std.build.Builder;
 const Pkg = std.build.Pkg;
 
+const pkgs = @import("deps.zig").pkgs;
+
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
 
     var tests = b.addTest("src/main.zig");
     tests.setBuildMode(mode);
 
-    tests.addPackage(.{
-        .name = "hzzp",
-        .path = "lib/hzzp/src/main.zig",
-        .dependencies = null,
-    });
+    inline for (std.meta.fields(@TypeOf(pkgs))) |field| {
+        tests.addPackage(@field(pkgs, field.name));
+    }
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&tests.step);
